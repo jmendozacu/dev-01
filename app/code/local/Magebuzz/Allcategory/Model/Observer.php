d6689bfcb3d1505cc51f6b53428331df968e0c23
thomaspham <thieupq@azebiz.com>
2016-02-25 09:42:35 +0700

    IND-34: Part1: Landing cat

---
+++ app/code/local/Magebuzz/Allcategory/Model/Observer.php
@@ -0,0 +1,15 @@
+<?php
+class Magebuzz_Allcategory_Model_Observer {
+  public function categoryViewLoadBefore(Varien_Event_Observer $observer){
+		/** @var $category */
+    $category = Mage::registry('current_category');
+		if ($category instanceof Mage_Catalog_Model_Category) {
+			/** @var $layout Mage_Core_Model_Layout */
+			$update = Mage::getSingleton('core/layout')->getUpdate();
+			$params = Mage::app()->getRequest()->getParams();
+			if ($category->hasChildren() && !isset($params['p'])) {
+				$update->addHandle('catalog_category_layered_haschildren');
+			}
+		}
+  }
+}
