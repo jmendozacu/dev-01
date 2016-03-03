946ac799c3bb84bf156212a4b460a74cdd753643
thomaspham <thieupq@azebiz.com>
2016-03-03 18:10:22 +0700

    IND-34

--- app/code/local/Magebuzz/Allcategory/Model/Observer.php
+++ app/code/local/Magebuzz/Allcategory/Model/Observer.php
@@ -3,7 +3,7 @@ class Magebuzz_Allcategory_Model_Observer {
   public function categoryViewLoadBefore(Varien_Event_Observer $observer){
 		/** @var $category */
     $category = Mage::registry('current_category');
-		if ($category instanceof Mage_Catalog_Model_Category) {
+		if ($category instanceof Mage_Catalog_Model_Category && !Mage::registry('current_product')) {
 			/** @var $layout Mage_Core_Model_Layout */
 			$update = Mage::getSingleton('core/layout')->getUpdate();
 			$params = Mage::app()->getRequest()->getParams();
