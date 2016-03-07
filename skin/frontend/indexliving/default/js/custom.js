b0602d2386fd1d76d28764208fd399453f2a3ffb
thomaspham <thieupq@azebiz.com>
2016-03-07 10:07:08 +0700

    MSS-35

--- skin/frontend/indexliving/default/js/custom.js
+++ skin/frontend/indexliving/default/js/custom.js
@@ -1,3 +1,39 @@
+function incQty(inputId){
+	var qty = parseInt(jQuery('#' + inputId).val());
+	jQuery('#' + inputId).val(qty + 1);
+	applyChangeCheckoutCartQty(inputId);
+	return false;
+}
+
+function decQty(inputId){
+	var qty = parseInt(jQuery('#' + inputId).val());
+	if(qty > 1){
+		jQuery('#' + inputId).val(qty - 1);
+	}
+	applyChangeCheckoutCartQty(inputId);
+	return false;
+}
+function applyChangeCheckoutCartQty(inputId){
+	jQuery('#apply_' + inputId).show();
+}
+
+/**
+	* Magebuzz / Buzzthemes
+	* Popup sharing
+*/
+
+function ss_plugin_loadpopup_js(em){
+	var shareurl=em.href;
+	var top = (screen.availHeight - 500) / 2;
+	var left = (screen.availWidth - 500) / 2;
+	var popup = window.open(
+			shareurl,
+			'social sharing',
+			'width=550,height=420,left='+ left +',top='+ top +',location=0,menubar=0,toolbar=0,status=0,scrollbars=1,resizable=1'
+	);
+	return false;
+}
+
 jQuery(document).ready(function () {
 
   /* language */
@@ -52,5 +88,7 @@ jQuery(document).ready(function () {
     scrollTop: 0
   }, 500);
   });
-  
+  jQuery('#shareProductLink').click(function() {
+		jQuery('#shareproduct-content').toggle("slide",{ direction: "right" }, 500);
+	});
 });
