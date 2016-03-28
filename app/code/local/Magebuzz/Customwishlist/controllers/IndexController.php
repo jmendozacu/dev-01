93a33c3b2cda4f4ec18c9fac453171b0b65ff504
trinhnv <trinhnv@outlook.com>
2016-03-28 16:56:41 +0700

    IND-65: issue log visitor

--- app/code/local/Magebuzz/Customwishlist/controllers/IndexController.php
+++ app/code/local/Magebuzz/Customwishlist/controllers/IndexController.php
@@ -11,7 +11,6 @@ class Magebuzz_Customwishlist_IndexController extends Mage_Wishlist_IndexControl
 	 */
 	public function compareAction()
 	{
-
 		$response = array();
 		if ($productId = (int) $this->getRequest()->getParam('product'))
 		{
@@ -24,20 +23,15 @@ class Magebuzz_Customwishlist_IndexController extends Mage_Wishlist_IndexControl
 				Mage::getSingleton('catalog/product_compare_list')->addProduct($product);
 				$response['status'] = 'SUCCESS';
 				$response['message'] = $this->__('The product %s has been added to comparison list.', Mage::helper('core')->escapeHtml($product->getName()));
-				$_productCollection = Mage::helper('catalog/product_compare')->getItemCollection()->count();
+//				$_productCollection = Mage::helper('catalog/product_compare')->getItemCollection()->count();
 
-				if($_productCollection >=1){
-					$response['popup'] = $this->getLayout()->createBlock('catalog/product_compare_list')->setTemplate('catalog/product/compare/list.phtml')->toHtml();
+					$response['bar'] = $this->getLayout()->createBlock('catalog/product_compare_sidebar')->setTemplate('catalog/product/compare/sidebar.phtml')->toHtml();
 					$this->getResponse()->setBody(Mage::helper('core')->jsonEncode($response));
-					return;
-				}else{
-					$this->getResponse()->setBody(Mage::helper('core')->jsonEncode($response));
-					return;
-				}
 
 			}
 		}
 	}
+
 
 	protected function _getWishlist()
 	{
