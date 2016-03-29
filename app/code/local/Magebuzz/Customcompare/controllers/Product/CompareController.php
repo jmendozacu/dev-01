29ec2d1d93b4c1e24a79a83d9737ae000cd64693
trinhnv <trinhnv@outlook.com>
2016-03-29 10:18:22 +0700

    IND-65: fix issue compare

--- app/code/local/Magebuzz/Customcompare/controllers/Product/CompareController.php
+++ app/code/local/Magebuzz/Customcompare/controllers/Product/CompareController.php
@@ -4,7 +4,6 @@ class MageBuzz_Customcompare_Product_CompareController extends Mage_Catalog_Prod
 
     public function addAction()
     {
-
         $response = array();
         if ($productId = (int) $this->getRequest()->getParam('product'))
         {
@@ -18,16 +17,89 @@ class MageBuzz_Customcompare_Product_CompareController extends Mage_Catalog_Prod
                         Mage::getSingleton('catalog/product_compare_list')->addProduct($product);
                         $response['status'] = 'SUCCESS';
                         $response['message'] = $this->__('The product %s has been added to comparison list.', Mage::helper('core')->escapeHtml($product->getName()));
-                        $response['name'] =  Mage::helper('core')->escapeHtml($product->getName());
-                        $response['url'] = $product->getProductUrl();
+                        Mage::register('referrer_url', $this->_getRefererUrl());
+                        Mage::helper('catalog/product_compare')->calculate();
+                        Mage::dispatchEvent('catalog_product_compare_add_product', array('product' => $product));
+                        $this->loadLayout();
                         $response['bar'] = $this->getLayout()->createBlock('catalog/product_compare_sidebar')->setTemplate('catalog/product/compare/sidebar.phtml')->toHtml();
-                        $this->getResponse()->setBody(Mage::helper('core')->jsonEncode($response));
                     }
                     catch (Exception $e) {
                         echo $e->getMessage();
-                        die('aa');
                     }
                 }
+
         }
+        $this->getResponse()->setBody(Mage::helper('core')->jsonEncode($response));
+        return;
+    }
+
+    public function removeAction()
+    {
+        $response = array();
+        if ($productId = (int) $this->getRequest()->getParam('product')) {
+            $product = Mage::getModel('catalog/product')
+                ->setStoreId(Mage::app()->getStore()->getId())
+                ->load($productId);
+
+            if($product->getId()) {
+                /** @var $item Mage_Catalog_Model_Product_Compare_Item */
+                $item = Mage::getModel('catalog/product_compare_item');
+                if(Mage::getSingleton('customer/session')->isLoggedIn()) {
+                    $item->addCustomerData(Mage::getSingleton('customer/session')->getCustomer());
+                } elseif ($this->_customerId) {
+                    $item->addCustomerData(
+                        Mage::getModel('customer/customer')->load($this->_customerId)
+                    );
+                } else {
+                    $item->addVisitorId(Mage::getSingleton('log/visitor')->getId());
+                }
+
+                $item->loadByProduct($product);
+
+                if($item->getId()) {
+                    $item->delete();
+                    Mage::dispatchEvent('catalog_product_compare_remove_product', array('product'=>$item));
+                    Mage::helper('catalog/product_compare')->calculate();
+                    $this->loadLayout();
+                    $response['bar'] = $this->getLayout()->createBlock('catalog/product_compare_sidebar')->setTemplate('catalog/product/compare/sidebar.phtml')->toHtml();
+                }
+            }
+        }
+        $response['status'] = 'SUCCESS';
+        $response['message'] = $this->__('The product %s has been added to comparison list.', Mage::helper('core')->escapeHtml($product->getName()));
+        $this->getResponse()->setBody(Mage::helper('core')->jsonEncode($response));
+        return;
+        }
+
+    public function clearAction()
+    {
+
+        $items = Mage::getResourceModel('catalog/product_compare_item_collection');
+
+        if (Mage::getSingleton('customer/session')->isLoggedIn()) {
+            $items->setCustomerId(Mage::getSingleton('customer/session')->getCustomerId());
+        } elseif ($this->_customerId) {
+            $items->setCustomerId($this->_customerId);
+        } else {
+            $items->setVisitorId(Mage::getSingleton('log/visitor')->getId());
+        }
+
+        /** @var $session Mage_Catalog_Model_Session */
+        $session = Mage::getSingleton('catalog/session');
+
+        try {
+            $items->clear();
+            $response['status'] = 'SUCCESS';
+            $response['message'] = $this->__('The comparison list was cleared.');
+            Mage::helper('catalog/product_compare')->calculate();
+            $this->loadLayout();
+            $response['bar'] = $this->getLayout()->createBlock('catalog/product_compare_sidebar')->setTemplate('catalog/product/compare/sidebar.phtml')->toHtml();
+        } catch (Mage_Core_Exception $e) {
+            $session->addError($e->getMessage());
+        } catch (Exception $e) {
+            $session->addException($e, $this->__('An error occurred while clearing comparison list.'));
+        }
+        $this->getResponse()->setBody(Mage::helper('core')->jsonEncode($response));
+        return;
     }
 }
