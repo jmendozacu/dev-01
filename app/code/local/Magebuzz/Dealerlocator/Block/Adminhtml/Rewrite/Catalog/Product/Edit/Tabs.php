66731ab9511dbc5a9d0838e1ca5c8eb661591750
trinhnv <trinhnv@outlook.com>
2016-04-04 16:20:29 +0700

    IND-196: fix issue

--- app/code/local/Magebuzz/Dealerlocator/Block/Adminhtml/Rewrite/Catalog/Product/Edit/Tabs.php
+++ app/code/local/Magebuzz/Dealerlocator/Block/Adminhtml/Rewrite/Catalog/Product/Edit/Tabs.php
@@ -2,11 +2,10 @@
 class Magebuzz_Dealerlocator_Block_Adminhtml_Rewrite_Catalog_Product_Edit_Tabs extends Mage_Adminhtml_Block_Catalog_Product_Edit_Tabs{ 
   protected function _prepareLayout(){
     parent::_prepareLayout();
-    
+
     //add new tab
 		$params = $this->getRequest()->getParams();
-		
-		if(isset($params['type'])){
+		if(isset($params['type'])||isset($params['id'])){
 			$url = Mage::helper('adminhtml')->getUrl('dealerlocatoradmin/adminhtml_productassigndealer/index', array('_current' => true));
 			$this->addTab('dealer', array(
 										'label'     => Mage::helper('catalog')->__('Dealer'),
