ded6bfb60a5db70680ffa426e01095c7f27b82e9
Minh do <minhdo@shopstack.asia>
2016-04-11 17:03:29 +0700

    IND-221: add new attribute customer address  (customer_mobile) write js for validation input

---
+++ app/code/local/Magebuzz/Customaddress/sql/customaddress_setup/mysql4-upgrade-0.1.3-0.1.4.php
@@ -0,0 +1,47 @@
+<?php
+$installer = $this;
+$installer->startSetup();
+
+$setup = new Mage_Eav_Model_Entity_Setup('core_setup');
+
+$setup->updateAttribute(
+    'customer_address',
+    'telephone',
+    'is_required',
+    0
+);
+$setup->addAttribute("customer_address", "customer_mobile",  array(
+    "type"     => "varchar",
+    "backend"  => "",
+    "label"    => "Mobile",
+    "input"    => "text",
+    "source"   => "",
+    "visible"  => true,
+    "required" => true,
+    "default" => "",
+    "frontend" => "",
+    "unique"     => false,
+    "note"       => ""
+
+));
+
+$attribute   = Mage::getSingleton("eav/config")->getAttribute("customer_address", "customer_mobile");
+
+
+$used_in_forms=array();
+
+$used_in_forms[]="adminhtml_customer_address";
+$used_in_forms[]="customer_register_address";
+$used_in_forms[]="customer_address_edit";
+$attribute->setData("used_in_forms", $used_in_forms)
+    ->setData("is_used_for_customer_segment", true)
+    ->setData("is_system", 0)
+    ->setData("is_user_defined", 1)
+    ->setData("is_visible", 1)
+    ->setData("sort_order", 100)
+;
+$attribute->save();
+
+
+
+$installer->endSetup();
