5fb2c467549351977e8ece18f71e970183e4138b
thomaspham <thieupq@azebiz.com>
2016-04-19 00:23:42 +0700

    IND-267

--- skin/frontend/indexliving/default/js/stickyfilter.js
+++ skin/frontend/indexliving/default/js/stickyfilter.js
@@ -2,15 +2,15 @@ jQuery(document).ready(function () {
 	var layerNavElementHeight = jQuery('.block-layered-nav').outerHeight();
 	var layerNavPos = jQuery('.block-layered-nav').offset().top;
 	var layerNavLastElementPos = jQuery('.block-layered-nav dt.filter_sticky').offset().top;
-	var toolbarBottomPos = jQuery('.toolbar-bottom').offset().top;
-	var finalDes = toolbarBottomPos - jQuery('.block-layered-nav dd.last').outerHeight();
 	jQuery(this).scroll(function() {
 		var scrollTopPos = jQuery(this).scrollTop();
 		var screenBottomPos = screen.availHeight + scrollTopPos;
 		var layerNavBottomPos = layerNavElementHeight + layerNavPos;
 		var winScrollTopTranform = scrollTopPos - layerNavLastElementPos;
 		var transformYValue = "translateY("+scrollTopPos+"px)";
-		if(screenBottomPos >= layerNavBottomPos && scrollTopPos <= finalDes) {
+		var footerBeforePos = jQuery('.footer-before-container').offset().top - layerNavPos;
+		var toolbarBottomPos = jQuery('.toolbar-bottom').offset().top;
+		if(screenBottomPos >= layerNavBottomPos && scrollTopPos <= footerBeforePos) {
 			transformYValue = "translateY("+winScrollTopTranform+"px)";
 			jQuery('.block-layered-nav').css('transform', transformYValue);
 		}else {
