e02a7455240cb9ed10052bf5ebb501dfa4712946
trinhnv <trinhnv@outlook.com>
2016-05-10 12:08:07 +0700

    IND-341: edit email in my account

--- app/code/local/Magebuzz/RewriteCustomer/controllers/AccountController.php
+++ app/code/local/Magebuzz/RewriteCustomer/controllers/AccountController.php
@@ -2,6 +2,86 @@
 require_once Mage::getModuleDir('controllers', 'Mage_Customer').DS.'AccountController.php';
 class Magebuzz_RewriteCustomer_AccountController extends Mage_Customer_AccountController
 {
+    public function editPostAction()
+    {
+        if (!$this->_validateFormKey()) {
+            return $this->_redirect('*/*/edit');
+        }
+
+        if ($this->getRequest()->isPost()) {
+            /** @var $customer Mage_Customer_Model_Customer */
+            $customer = $this->_getSession()->getCustomer();
+
+            /** @var $customerForm Mage_Customer_Model_Form */
+            $customerForm = $this->_getModel('customer/form');
+            $customerForm->setFormCode('customer_account_edit')
+                ->setEntity($customer);
+
+            $customerData = $customerForm->extractData($this->getRequest());
+
+            $errors = array();
+
+            $customerForm->compactData($customerData);
+            $errors = array();
+
+            // If password change was requested then add it to common validation scheme
+            if ($this->getRequest()->getParam('change_password')) {
+                $currPass   = $this->getRequest()->getPost('current_password');
+                $newPass    = $this->getRequest()->getPost('password');
+                $confPass   = $this->getRequest()->getPost('confirmation');
+
+                $oldPass = $this->_getSession()->getCustomer()->getPasswordHash();
+                if ( $this->_getHelper('core/string')->strpos($oldPass, ':')) {
+                    list($_salt, $salt) = explode(':', $oldPass);
+                } else {
+                    $salt = false;
+                }
+
+                if ($customer->hashPassword($currPass, $salt) == $oldPass) {
+                    if (strlen($newPass)) {
+                        /**
+                         * Set entered password and its confirmation - they
+                         * will be validated later to match each other and be of right length
+                         */
+                        $customer->setPassword($newPass);
+                        $customer->setPasswordConfirmation($confPass);
+                    } else {
+                        $errors[] = $this->__('New password field cannot be empty.');
+                    }
+                } else {
+                    $errors[] = $this->__('Invalid current password');
+                }
+            }
+
+            if (!empty($errors)) {
+                $this->_getSession()->setCustomerFormData($this->getRequest()->getPost());
+                foreach ($errors as $message) {
+                    $this->_getSession()->addError($message);
+                }
+                $this->_redirect('*/*/edit');
+                return $this;
+            }
+
+            try {
+                $customer->cleanPasswordsValidationData();
+                $customer->save();
+                $this->_getSession()->setCustomer($customer)
+                    ->addSuccess($this->__('The account information has been saved.'));
+
+                $this->_redirect('customer/account');
+                return;
+            } catch (Mage_Core_Exception $e) {
+                $this->_getSession()->setCustomerFormData($this->getRequest()->getPost())
+                    ->addError($e->getMessage());
+            } catch (Exception $e) {
+                $this->_getSession()->setCustomerFormData($this->getRequest()->getPost())
+                    ->addException($e, $this->__('Cannot save the customer.'));
+            }
+        }
+
+        $this->_redirect('*/*/edit');
+    }
+
     protected function _getCustomerErrors($customer)
     {
         $errors = array();
