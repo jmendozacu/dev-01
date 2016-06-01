26841fcc277bd90f24ff0f58a24cc537f9b87a91
thomaspham <thieu@azebiz.com>
2016-06-02 00:15:48 +0700

    IND-416: separate category image in list with category thumbnail field

---
+++ app/code/local/Magebuzz/Themecustom/sql/themecustom_setup/mysql4-upgrade-0.1.0-0.1.1.php
@@ -0,0 +1,26 @@
+<?php
+	$installer = $this;
+	$setup = new Mage_Eav_Model_Entity_Setup('core_setup');
+	$installer->startSetup();
+
+	$setup->addAttribute('catalog_category', 'category_thumbnail', array(
+			'type'             	=> 'varchar',
+			'group'             => '',
+			'backend' 					=> 'catalog/category_attribute_backend_image',
+			'frontend'          => '',
+			'label'             => 'Thumbnail',
+			'input'             => 'image',
+			'class'             => '',
+			'global'            => Mage_Catalog_Model_Resource_Eav_Attribute::SCOPE_GLOBAL,
+			'visible'           => true,
+			'required'          => false,
+			'user_defined'      => false,
+			'default'           => '0',
+			'searchable'        => false,
+			'filterable'        => true,
+			'comparable'        => false,
+			'visible_on_front'  => true,
+			'unique'            => false,
+			'group'             => 'General Information',    ));
+	$installer->endSetup();
+?>
