7f6ed6360d0ff6dab60b26d1f8be6fe92502d40c
sittisak.t <sittisak.t@marginframe.com>
2016-06-02 13:08:27 +0700

    IND-404 need to manage position for tag dealer

--- app/code/local/Magebuzz/Dealerlocator/Block/Dealerlocator.php
+++ app/code/local/Magebuzz/Dealerlocator/Block/Dealerlocator.php
@@ -180,6 +180,7 @@ class Magebuzz_Dealerlocator_Block_Dealerlocator extends Mage_Core_Block_Templat
     $dealerCollection = $this->_getDealers();
     $dealerIds = $dealerCollection->getColumnValues('dealerlocator_id');
     $tagCollection = Mage::getModel('dealerlocator/tag')->getCollection()->addFieldToFilter('dealer_id', array('in' => $dealerIds));
+    $tagCollection->getSelect()->order('tag', 'asc');
     $tags = array();
     if (count($tagCollection)) {
       foreach ($tagCollection as $tag) {
