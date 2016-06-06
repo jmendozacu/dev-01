0908bba065da1b7992b0a47feb8b90c1e93b6cc2
jirameth.p <jirament.pe@marginframe.com>
2016-06-06 18:38:03 +0700

    sync insert log

--- app/code/local/MarginFrame/Sync/Model/Cron/Price.php
+++ app/code/local/MarginFrame/Sync/Model/Cron/Price.php
@@ -4,130 +4,147 @@ class MarginFrame_Sync_Model_Cron_Price extends Mage_Core_Model_Abstract
 	
 	public function Run() {
 		// /var/interface/stock
-		$dir = Mage::getBaseDir('var').DS.'interface'.DS.'import'.DS.'price'.DS;
+		try {
 
-		$filename_log = "mgfsync_price.log";
-		// Tiw
-		// Create folder
-		if (!file_exists($dir)) {
-			$file = new Varien_Io_File();
-			$file->mkdir($dir);
-		}
+			$dir = Mage::getBaseDir('var').DS.'interface'.DS.'import'.DS.'price'.DS;
 
-		$dh  = opendir($dir);
+			$filename_log = "mgfsync_price.log";
+			// Tiw
+			// Create folder
+			if (!file_exists($dir)) {
+				$file = new Varien_Io_File();
+				$file->mkdir($dir);
+			}
 
-		while (false !== ($filename = readdir($dh))) {
-		    $files[] = $filename;
+			$dh  = opendir($dir);
 
-		    // find .trg file
-		    if(strrpos($filename, '.trg')) {
+			while (false !== ($filename = readdir($dh))) {
+			    $files[] = $filename;
 
-		    	// rename file .trg to .csv
-		    	$filenamecsv = str_replace('.trg', '.txt', $filename);
+			    // find .trg file
+			    if(strrpos($filename, '.trg')) {
 
-				$row = 1;
+			    	// rename file .trg to .csv
+			    	$filenamecsv = str_replace('.trg', '.txt', $filename);
 
-				if (($handle = fopen($dir.$filenamecsv, "r")) !== FALSE) {
-				    Mage::log('=========================================================', null, $filename_log);
-				    Mage::log('open file : '.$dir.$filenamecsv, null, $filename_log);
+					$row = 1;
 
-				    $csvdata = array();
-				    while (($data = fgetcsv($handle, 1000, "|")) !== FALSE) {
+					if (($handle = fopen($dir.$filenamecsv, "r")) !== FALSE) {
+					    Mage::log('=========================================================', null, $filename_log);
+					    Mage::log('open file : '.$dir.$filenamecsv, null, $filename_log);
 
-				        $sku = trim($data[0]);
-				        $csvdata[$sku]['price'] = trim($data[1]);
-				        $csvdata[$sku]['special_price'] = trim($data[2]);
-				        $csvdata[$sku]['special_from_date'] = trim($data[3]);
-				        $csvdata[$sku]['special_to_date'] = trim($data[4]);
-				    }
+					    $csvdata = array();
+					    while (($data = fgetcsv($handle, 1000, "|")) !== FALSE) {
 
-				    $storeId = 0;
-				   
-		    		
-		    		$product = null;
-		    		Mage::app()->setCurrentStore(Mage_Core_Model_App::ADMIN_STORE_ID);
-				    foreach ($csvdata as $sku => $data) {
-				    	$product = Mage::getModel('catalog/product')
-				    	->loadByAttribute('sku', $sku);
-				    	if ($product->getId()) {
-				    		Mage::log('found sku : '.$sku, null, $filename_log);
+					        $sku = trim($data[0]);
+					        $csvdata[$sku]['price'] = trim($data[1]);
+					        $csvdata[$sku]['special_price'] = trim($data[2]);
+					        $csvdata[$sku]['special_from_date'] = trim($data[3]);
+					        $csvdata[$sku]['special_to_date'] = trim($data[4]);
+					    }
 
-				    		if ($data['price'] != 'Unset') {
-				    			$product->setPrice(number_format($data['price'],2));
+					    $storeId = 0;
+					   
+			    		
+			    		$product = null;
+			    		Mage::app()->setCurrentStore(Mage_Core_Model_App::ADMIN_STORE_ID);
+					    foreach ($csvdata as $sku => $data) {
+					    	$product = Mage::getModel('catalog/product')
+					    	->loadByAttribute('sku', $sku);
+					    	if ($product->getId()) {
+					    		Mage::log('found sku : '.$sku, null, $filename_log);
 
-				    			if (strtolower($data['special_price']) != 'unset') {
-					    			$product->setSpecialPrice(number_format($data['special_price'],2));
+					    		if ($data['price'] != 'Unset') {
+					    			$product->setPrice(number_format($data['price'],2));
 
-					    			$product->setSpecialFromDate(date('Y-m-d',strtotime($data['special_from_date'])));
-									$product->setSpecialFromDateIsFormated(true);
+					    			if (strtolower($data['special_price']) != 'unset') {
+						    			$product->setSpecialPrice(number_format($data['special_price'],2));
 
-									$product->setSpecialToDate(date('Y-m-d',strtotime($data['special_to_date'])));
-									$product->setSpecialToDateIsFormated(true);
+						    			$product->setSpecialFromDate(date('Y-m-d',strtotime($data['special_from_date'])));
+										$product->setSpecialFromDateIsFormated(true);
+
+										$product->setSpecialToDate(date('Y-m-d',strtotime($data['special_to_date'])));
+										$product->setSpecialToDateIsFormated(true);
+						    		}else{
+						    			$product->setSpecialPrice(0);
+						    			
+						    			$product->setSpecialFromDate(false);
+										$product->setSpecialFromDateIsFormated(true);
+
+										$product->setSpecialToDate(false);
+										$product->setSpecialToDateIsFormated(true);
+						    		}
 					    		}else{
-					    			$product->setSpecialPrice(0);
-					    			
-					    			$product->setSpecialFromDate(false);
-									$product->setSpecialFromDateIsFormated(true);
-
-									$product->setSpecialToDate(false);
-									$product->setSpecialToDateIsFormated(true);
+					    			$product->setStatus(2);
 					    		}
-				    		}else{
-				    			$product->setStatus(2);
-				    		}
 
-				    		// call save() method to save your product with updated data
-							try{
-								$product->save();
-							} catch (Exception $ex) {
-								// handle the error here!!
-								Mage::log('error sku : '.$sku.'-'.$ex->getMessage(), null, $filename_log);
+					    		// call save() method to save your product with updated data
+								try{
+									$product->save();
+									$log = 'Success';
+								} catch (Exception $ex) {
+									// handle the error here!!
+									Mage::log('error sku : '.$sku.'-'.$ex->getMessage(), null, $filename_log);
+								}
+								
+					    		
+					    	} else {
+								 Mage::log("SKU not found : ".$sku, null, $filename_log);
 							}
 							
-				    		
-				    	} else {
-							 Mage::log("SKU not found : ".$sku, null, $filename_log);
-						}
+					    	
+					    }
+					    unset($product);
+						fclose($handle);
 						
-				    	
-				    }
-				    unset($product);
-					fclose($handle);
-					
-					Mage::log('close file : '.$dir.$filenamecsv, null, $filename_log);
+						Mage::log('close file : '.$dir.$filenamecsv, null, $filename_log);
 
-					// moved file to completed path
-					$newdir = Mage::getBaseDir('var').DS.'interface'.DS.'import'.DS.'price'.DS.'save'.DS;
+						// moved file to completed path
+						$newdir = Mage::getBaseDir('var').DS.'interface'.DS.'import'.DS.'price'.DS.'save'.DS;
 
-					// Tiw
-					// Create folder
-					if (!file_exists($newdir)) {
-						$file = new Varien_Io_File();
-						$file->mkdir($newdir);
+						// Tiw
+						// Create folder
+						if (!file_exists($newdir)) {
+							$file = new Varien_Io_File();
+							$file->mkdir($newdir);
+						}
+
+						// Mage::log($newdir);
+						unlink($dir.$filename);
+						rename($dir.$filenamecsv, $newdir.$filenamecsv);
+
+						// Tiw
+						// check to remove file
+						if (!file_exists($dir.$filename)) {
+							Mage::log('removed : '.$dir.$filename, null, $filename_log);
+						}else{
+							Mage::log('can not removed : '.$dir.$filename, null, $filename_log);
+						}
+
+						// check to move file
+						if (!file_exists($dir.$filenamecsv)) {
+							Mage::log('moved to completed : '.$newdir.$filenamecsv, null, $filename_log);
+						}else{
+							Mage::log('can not moved : '.$newdir.$filenamecsv, null, $filename_log);
+						}
 					}
 
-					// Mage::log($newdir);
-					unlink($dir.$filename);
-					rename($dir.$filenamecsv, $newdir.$filenamecsv);
+			    }
+			}
 
-					// Tiw
-					// check to remove file
-					if (!file_exists($dir.$filename)) {
-						Mage::log('removed : '.$dir.$filename, null, $filename_log);
-					}else{
-						Mage::log('can not removed : '.$dir.$filename, null, $filename_log);
-					}
-
-					// check to move file
-					if (!file_exists($dir.$filenamecsv)) {
-						Mage::log('moved to completed : '.$newdir.$filenamecsv, null, $filename_log);
-					}else{
-						Mage::log('can not moved : '.$newdir.$filenamecsv, null, $filename_log);
-					}
-				}
-
-		    }
+		} catch(Exception $ex) {
+			$log = 'Error : '.$ex->getMessage();
 		}
+
+		$resource = Mage::getSingleton('core/resource');
+    	$readConnection = $resource->getConnection('core_read');
+    	$writeConnection = $resource->getConnection('core_write');
+
+	    $query = "
+	    	INSERT INTO `tbl_sync_log` (sync_type, created_at, log)
+	    	VALUES ('Retail Price', NOW(), '$log')
+	    ";
+	    $writeConnection->query($query);
 
    	}
 }
