a962f3d8555d9deec934ab429128d3a61c4479f1
Nguyen Thuy Ninh <ninhnt@azebiz.com>
2017-03-08 11:33:26 +0700

    INDC-7: fix bug

--- app/code/local/Magpleasure/Blog/Block/Content/Category/List.php
+++ app/code/local/Magpleasure/Blog/Block/Content/Category/List.php
@@ -7,24 +7,5 @@
 
 class Magpleasure_Blog_Block_Content_Category_List extends Magpleasure_Blog_Block_Content_Category
 {
-    protected $_collection;
 
-    public function getCollection()
-    {
-        if (!$this->_collection){
-            /** @var Magpleasure_Blog_Model_Mysql4_Category_Collection $collection  */
-            $collection = Mage::getModel('mpblog/category')->getCollection();
-
-            $collection->addFieldToFilter('status', Magpleasure_Blog_Model_Category::STATUS_ENABLED);
-
-            if (!Mage::app()->isSingleStoreMode()){
-                $collection->addStoreFilter(Mage::app()->getStore()->getId());
-            }
-
-            $collection->setSortOrder('asc');
-
-            $this->_collection = $collection;
-        }
-        return $this->_collection;
-    }
 }
