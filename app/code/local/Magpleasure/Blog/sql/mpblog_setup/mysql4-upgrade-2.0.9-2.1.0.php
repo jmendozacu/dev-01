20ace8a4786b996b4a20d51304a258e400713674
JackieNguyen <duong@azebiz.com>
2017-03-16 15:39:49 +0700

    INDC-53 : show sidebar sub-category

---
+++ app/code/local/Magpleasure/Blog/sql/mpblog_setup/mysql4-upgrade-2.0.9-2.1.0.php
@@ -0,0 +1,8 @@
+<?php
+$installer = $this;
+$installer->startSetup();
+$installer->run("
+ALTER TABLE `{$this->getTable('mp_blog_categories')}`
+  ADD COLUMN `category_is_landing` int(11) NULL default 0
+");
+$installer->endSetup();
