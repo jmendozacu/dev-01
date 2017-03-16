13204b56e9943045d37c558d13eb30d05c57d9b2
Nguyen Thuy Ninh <ninhnt@azebiz.com>
2017-03-16 17:23:50 +0700

    INDC-36 and INDC-37

--- app/code/local/Magpleasure/Blog/sql/mpblog_setup/mysql4-upgrade-2.1.0-2.1.1.php
+++ app/code/local/Magpleasure/Blog/sql/mpblog_setup/mysql4-upgrade-2.1.0-2.1.1.php
@@ -2,7 +2,7 @@
 $installer = $this;
 $installer->startSetup();
 $installer->run("
-ALTER TABLE `{$this->getTable('mp_blog_categories')}`ctegory
+ALTER TABLE `{$this->getTable('mp_blog_categories')}`
   ADD COLUMN `category_is_landing_project` int(11) NULL default 0
 ");
 $installer->endSetup();
