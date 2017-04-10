b27b09a82a8ab91c2ec2c508b4716a767324247a
hangvu <hang@azebiz.com>
2017-04-11 05:53:26 +0700

    INDC-98 FAQ Extension 1. Can't show image of answer on front-end

--- app/code/community/Magebuzz/Faq/Block/Adminhtml/Faq/Edit/Tab/Form.php
+++ app/code/community/Magebuzz/Faq/Block/Adminhtml/Faq/Edit/Tab/Form.php
@@ -1,12 +1,35 @@
 <?php
 
 class Magebuzz_Faq_Block_Adminhtml_Faq_Edit_Tab_Form extends Mage_Adminhtml_Block_Widget_Form
+implements Mage_Adminhtml_Block_Widget_Tab_Interface
 {
+    protected function _helper()
+    {
+        return Mage::helper('faq');
+    }
+    public function getTabLabel()
+    {
+        return $this->_helper()->__("Answer");
+    }
+    public function getTabTitle()
+    {
+        return $this->_helper()->__("Content");
+    }
+    public function canShowTab()
+    {
+        return true;
+    }
+    public function isHidden()
+    {
+        return false;
+    }
     protected function _prepareForm()
     {
+
         $form = new Varien_Data_Form();
         $this->setForm($form);
         $fieldset = $form->addFieldset('faq_form', array('legend' => Mage::helper('faq')->__('General Information')));
+        $fieldset->addType('mp_full_editor', 'Magpleasure_Blog_Block_Adminhtml_Widget_Form_Wysiwyg');
 
         $model = Mage::registry('faq_data');
         $fieldset->addField('question', 'text', array(
@@ -78,13 +101,13 @@ class Magebuzz_Faq_Block_Adminhtml_Faq_Edit_Tab_Form extends Mage_Adminhtml_Bloc
             $editorConfig = array();
         }
 
-        $fieldset->addField('answer', 'editor', array(
-            'name' => 'answer',
-            'label' => Mage::helper('faq')->__('Answer'),
-            'title' => Mage::helper('faq')->__('Answer'),
-            'style' => 'height:12em; width: 50em;',
-            'required' => true,
-            'config' => $editorConfig,
+        $fieldset->addField('answer', 'mp_full_editor', array(
+          'name'      => 'answer',
+          'label'     => Mage::helper('faq')->__('Answer'),
+          'title'     => Mage::helper('faq')->__('Answer'),
+          'style'     => 'min-width: 36em; width: 100%;',
+          'required'  => true,
+          'min_height'=> 300,
         ));
 
         if (Mage::getSingleton('adminhtml/session')->getFaqData()) {
