fb48b391c097a5fab0a262653ade5b53e4ea2d58
sittisak.t <sittisak.t@marginframe.com>
2017-08-15 18:41:55 +0700

    fixed error 500 when direct to url blog ie. https://www.indexlivingmall.com/blog/promotion-the-way-to-say-love-mom.html

--- app/code/local/Magpleasure/Blog/Block/Content/Post.php
+++ app/code/local/Magpleasure/Blog/Block/Content/Post.php
@@ -104,39 +104,42 @@ class Magpleasure_Blog_Block_Content_Post extends Magpleasure_Blog_Block_Content
 //                'link' => $this->_helper()->_url()->getUrl(),
 //            ));
                 $category = $this->getCategory();
-                $category_is_landing_homedecor = $category->getData('category_is_landing');
-                $category_is_landing_indexproject = $category->getData('category_is_landing_project');
-                $category_style = $this->getCategory()->getData('category_style');
-//
-                if(!$category_is_landing_homedecor && $category_style == Magpleasure_Blog_Model_Categorystyle::HOME_DECOR){
-                    $parentCategorys = Mage::helper('mpblog')->getParentCategory($category_style);
-                    if($parentCategorys->getSize()){
-                        foreach ($parentCategorys as $parentCategory){
-                            $breadcrumbs->addCrumb($parentCategory->getUrlKey(), array(
-                              'label' => $parentCategory->getName(),
-                              'title' => $parentCategory->getName(),
-                              'link' => Mage::getBaseUrl('web').'blog/category/'.$parentCategory->getUrlKey(),
-                            ));
+                if($category){
+                    $category_is_landing_homedecor = $category->getData('category_is_landing');
+                    $category_is_landing_indexproject = $category->getData('category_is_landing_project');
+                    $category_style = $this->getCategory()->getData('category_style');
+    //
+                    if(!$category_is_landing_homedecor && $category_style == Magpleasure_Blog_Model_Categorystyle::HOME_DECOR){
+                        $parentCategorys = Mage::helper('mpblog')->getParentCategory($category_style);
+                        if($parentCategorys->getSize()){
+                            foreach ($parentCategorys as $parentCategory){
+                                $breadcrumbs->addCrumb($parentCategory->getUrlKey(), array(
+                                  'label' => $parentCategory->getName(),
+                                  'title' => $parentCategory->getName(),
+                                  'link' => Mage::getBaseUrl('web').'blog/category/'.$parentCategory->getUrlKey(),
+                                ));
+                            }
                         }
                     }
-                }
-                if(!$category_is_landing_indexproject && $category_style == Magpleasure_Blog_Model_Categorystyle::INDEX_PROJECT){
-                    $parentCategoryIndexprojects = Mage::helper('mpblog')->getParentCategoryIndexproject($category_style);
-                    if($parentCategoryIndexprojects->getSize()){
-                        foreach ($parentCategoryIndexprojects as $parentCategoryIndexproject){
-                            $breadcrumbs->addCrumb($parentCategoryIndexproject->getUrlKey(), array(
-                              'label' => $parentCategoryIndexproject->getName(),
-                              'title' => $parentCategoryIndexproject->getName(),
-                              'link' => Mage::getBaseUrl('web').'blog/category/'.$parentCategoryIndexproject->getUrlKey(),
-                            ));
+                    if(!$category_is_landing_indexproject && $category_style == Magpleasure_Blog_Model_Categorystyle::INDEX_PROJECT){
+                        $parentCategoryIndexprojects = Mage::helper('mpblog')->getParentCategoryIndexproject($category_style);
+                        if($parentCategoryIndexprojects->getSize()){
+                            foreach ($parentCategoryIndexprojects as $parentCategoryIndexproject){
+                                $breadcrumbs->addCrumb($parentCategoryIndexproject->getUrlKey(), array(
+                                  'label' => $parentCategoryIndexproject->getName(),
+                                  'title' => $parentCategoryIndexproject->getName(),
+                                  'link' => Mage::getBaseUrl('web').'blog/category/'.$parentCategoryIndexproject->getUrlKey(),
+                                ));
+                            }
                         }
                     }
+                
+                    $breadcrumbs->addCrumb($this->getCategory()->getUrlKey(), array(
+                      'label' => $this->getCategory()->getName(),
+                      'title' => $this->getCategory()->getName(),
+                      'link' => Mage::getBaseUrl('web').'blog/category/'.$this->getCategory()->getUrlKey(),
+                    ));
                 }
-                $breadcrumbs->addCrumb($this->getCategory()->getUrlKey(), array(
-                  'label' => $this->getCategory()->getName(),
-                  'title' => $this->getCategory()->getName(),
-                  'link' => Mage::getBaseUrl('web').'blog/category/'.$this->getCategory()->getUrlKey(),
-                ));
 
                 $breadcrumbs->addCrumb('post', array(
                   'label' => $this->getTitle(),
